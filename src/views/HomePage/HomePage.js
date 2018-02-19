1c18958308202cb936aa784b1cfb306a666af294
William Antony <william@pathadan.com>
2018-02-19 00:42:36 -0600

    Create Homepage view

---
+++ src/views/HomePage/HomePage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ScreenView from '../../components/ScreenView/ScreenView';
+
+class HomePage extends Component {
+
+  render() {
+    return (
+      <ScreenView>
+
+        
+
+      </ScreenView>
+    );
+  }
+
+}
+
+export default HomePage;
