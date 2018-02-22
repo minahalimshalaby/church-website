0706de40b0494154f339c8ba7bb78d765ec2b80c
William Antony <william@pathadan.com>
2018-02-21 23:06:45 -0600

    Added title attribute to iframe

--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -8,12 +8,12 @@ class Cover extends Component {
     const video = this.props.video + '?rel=0&showinfo=0&controls=0&modestbranding=1&iv_load_policy=3&autoplay=1&loop=1';
     return (
       <div className="Cover">
-        <iframe className="Cover__media Cover__media--video" src={ video } frameborder="0" allowfullscreen />
+        <iframe className="Cover__media Cover__media--video" src={ video } title="cover-video" frameborder="0" allowfullscreen />
         <div className="Cover__overlay Cover__overlay--gradient-top-bottom" />
         <div className="Cover__overlay Cover__overlay--gradient-diagonal" />
         <div className="Cover__overlay" />
         <div className="Cover__action">
-          <button>Play w/ sound</button>        
+          <button>Play w/ sound</button>
         </div>
       </div>
     );
