e802390e38ec307466c0cf2e07424dd97eb10550
William Antony <william@pathadan.com>
2018-03-10 13:54:29 -0600

    Created Section Component

--- frontend/src/views/HomePage/HomePage.js
+++ frontend/src/views/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ScreenView from '../../components/ScreenView/ScreenView';
 import Main from '../../components/Main/Main';
+import Section from '../../components/Section/Section';
 import Cover from '../../components/Cover/Cover';
 
 class HomePage extends Component {
@@ -12,6 +13,10 @@ class HomePage extends Component {
           
           <Cover image="http://www.ipachicago.org/wp-content/uploads/2017/10/church_image.png" />
           
+          <Section>
+            Hello
+          </Section>
+
         </Main>
       </ScreenView>
     );
