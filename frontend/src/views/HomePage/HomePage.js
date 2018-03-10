241a09558b77f89e001eb0ed0f42410725e09e9c
William Antony <william@pathadan.com>
2018-03-10 14:56:46 -0600

    Started with location & timing

--- frontend/src/views/HomePage/HomePage.js
+++ frontend/src/views/HomePage/HomePage.js
@@ -14,7 +14,30 @@ class HomePage extends Component {
           <Cover image="http://www.ipachicago.org/wp-content/uploads/2017/10/church_image.png" />
           
           <Section>
-            Hello
+            
+            <div className="Item">
+              <div className="Item__icon" />
+              <div className="Item__text">
+                <div className="item__label">Located at</div>
+                <div className="item__value">
+                  6200 West Foster Avenue,<br/>
+                  Chicago, IL - 60625
+                </div>
+                <div className="item__link">Get Direction</div>
+              </div>
+            </div>
+
+            <div className="Item">
+              <div className="Item__icon" />
+              <div className="Item__text">
+                <div className="item__label">Worship time</div>
+                <div className="item__value">
+                  Sunday: 11:00 AM to 12:00 PM<br/>
+                </div>
+                <div className="item__link">Upcoming Events</div>
+              </div>
+            </div>
+
           </Section>
 
         </Main>
