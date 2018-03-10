d98eb88a950341bb5a076a39f0675a7400ef0054
William Antony <william@pathadan.com>
2018-03-10 11:26:33 -0600

    Cleaned up Homepage Component

--- frontend/src/views/HomePage/HomePage.js
+++ frontend/src/views/HomePage/HomePage.js
@@ -8,17 +8,11 @@ class HomePage extends Component {
   render() {
     return (
       <ScreenView>
-
         <Main>
           
-          {/* <Cover video="https://www.youtube.com/embed/bMJ7KwqiO8c" /> */}
-          {/* <Cover video="https://www.youtube.com/embed/66tM1eDjAqI" /> */}
-          {/* <Cover image="https://img.youtube.com/vi/66tM1eDjAqI/maxresdefault.jpg" /> */}
-          {/* <Cover image="https://i.imgur.com/otNZnRFh.png" /> */}
           <Cover image="http://www.ipachicago.org/wp-content/uploads/2017/10/church_image.png" />
-
+          
         </Main>
-
       </ScreenView>
     );
   }
