e638f833f56b0beff7084db86df91595061430f4
William Antony <william@pathadan.com>
2018-03-10 23:37:10 -0600

    Created PrimaryInformation Component

---
+++ frontend/src/components/PrimaryInformation/PrimaryInformation.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './PrimaryInformation.css';
+
+class PrimaryInformation extends Component {
+
+  render() {
+    return (
+      <div className="PrimaryInformation">
+
+        <div className="PrimaryInformation__item">
+          <div className="PrimaryInformation__icon PrimaryInformation__icon--location" />
+          <div className="PrimaryInformation__text">
+            <div className="PrimaryInformation__label">Location</div>
+            <div className="PrimaryInformation__value">
+              6200 West Foster Avenue,<br/>
+              Chicago, IL - 60625
+            </div>
+          </div>
+        </div>
+
+        <div className="PrimaryInformation__item">
+          <div className="PrimaryInformation__icon PrimaryInformation__icon--timing" />
+          <div className="PrimaryInformation__text">
+            <div className="PrimaryInformation__label">Sunday</div>
+            <div className="PrimaryInformation__value">
+              09:45 AM - Malayalam Service<br/>
+              11:00 AM - English Service
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
+
+}
+
+export default PrimaryInformation;
--- frontend/src/views/HomePage/HomePage.js
+++ frontend/src/views/HomePage/HomePage.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+
 import ScreenView from '../../components/ScreenView/ScreenView';
 import Main from '../../components/Main/Main';
 import Section from '../../components/Section/Section';
+
 import Cover from '../../components/Cover/Cover';
+import PrimaryInformation from '../../components/PrimaryInformation/PrimaryInformation';
 
 class HomePage extends Component {
 
@@ -14,30 +17,7 @@ class HomePage extends Component {
           <Cover image="http://www.ipachicago.org/wp-content/uploads/2017/10/church_image.png" />
           
           <Section>
-            
-            <div className="Item">
-              <div className="Item__icon" />
-              <div className="Item__text">
-                <div className="item__label">Located at</div>
-                <div className="item__value">
-                  6200 West Foster Avenue,<br/>
-                  Chicago, IL - 60625
-                </div>
-                <div className="item__link">Get Direction</div>
-              </div>
-            </div>
-
-            <div className="Item">
-              <div className="Item__icon" />
-              <div className="Item__text">
-                <div className="item__label">Worship time</div>
-                <div className="item__value">
-                  Sunday: 11:00 AM to 12:00 PM<br/>
-                </div>
-                <div className="item__link">Upcoming Events</div>
-              </div>
-            </div>
-
+            <PrimaryInformation />
           </Section>
 
         </Main>
